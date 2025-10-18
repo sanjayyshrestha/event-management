79133fe2bb12f326c529612ff7be4404d7837da0
sanjayshrestha <sanjayshrestha772@gmail.com>
2025-10-18 14:45:11 +0545

    protect admin route

--- app/(admin)/edit-event/[id]/page.tsx
+++ app/(admin)/edit-event/[id]/page.tsx
@@ -2,13 +2,16 @@
 
 import { getSpecificEvent } from '@/actions/event'
 import { EventFormPage } from '@/components/EventForm'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import React from 'react'
 import type { Events } from '@/app/page'
+import { getRole } from '@/actions/user'
 type Event=Events[number]
 const EditEventPage = async ({params}:{
   params:Promise<{id:string}>
 }) => {
+  const role=await getRole();
+  if(role!=='ADMIN') redirect('/')
   const {id:eventId}=await params
   const event=await getSpecificEvent(eventId);
   if(!event) notFound()
